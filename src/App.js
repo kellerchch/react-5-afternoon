5b4d91b5a74377eacec1b8de969e42da61d8a22e
kellerchch <kellerchch@gmail.com>
2018-03-08 17:05:33 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,71 +1,72 @@
 import React, { Component } from 'react';
 import './App.css';
 import router from './router'
+import { connect } from 'react-redux';
 
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      //first view
-      loanType: 'Home Purchase',
-      propertyType: 'Single Family Home',
-      //second view
-      city: '',
-      //third view
-      propToBeUsedOn: '',
-      //fourth view
-      found: "false",
-      //fifth view
-      realEstateAgent: "false",
-      //sixth view
-      downPayment: 0,
-      cost: 0,
-      //seventh view
-      credit: '',
-      //eigth view
-      history: '',
-      //ninth view
-      addressOne: '',
-      addressTwo: '',
-      addressThree: '',
-      //tenth view
-      firstName: '',
-      lastName: '',
-      email: ''
-    }
-    //first view 
-    this.handleChangeLoanType = this.handleChangeLoanType.bind(this);
-    this.handleChangePropertyType = this.handleChangePropertyType.bind(this);
-    //second view
-    this.handleChangeCity = this.handleChangeCity.bind(this);
-    //third view
-    this.handleChangePropertyToBeUsedOn = this.handleChangePropertyToBeUsedOn.bind(this);
-    //fourth view
-    this.handleChangeFoundFalse = this.handleChangeFoundFalse.bind(this);
-    this.handleChangeFoundTrue = this.handleChangeFoundTrue.bind(this);
-    //fifth view
-    this.handleChangeRealEstateAgentTrue = this.handleChangeRealEstateAgentTrue.bind(this);
-    this.handleChangeRealEstateAgentFalse = this.handleChangeRealEstateAgentFalse.bind(this);
-    //sixth view
-    this.handleChangeUpdateDownPayment = this.handleChangeUpdateDownPayment.bind(this);
-    this.handleChangeUpdateCost = this.handleChangeUpdateCost.bind(this);
-    //seventh view
-    this.handleChangeCreditE = this.handleChangeCreditE.bind(this);
-    this.handleChangeCreditG = this.handleChangeCreditG.bind(this);
-    this.handleChangeCreditF = this.handleChangeCreditF.bind(this);
-    this.handleChangeCreditP = this.handleChangeCreditP.bind(this);
-    //eigth view
-    this.handleChangeUpdateHistory = this.handleChangeUpdateHistory.bind(this);
-    //ninth view
-    this.handleChangeAddressOne = this.handleChangeAddressOne.bind(this);
-    this.handleChangeAddressTwo = this.handleChangeAddressTwo.bind(this);
-    this.handleChangeAddressThree = this.handleChangeAddressThree.bind(this);
-    //tenth view
-    this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
-    this.handleChangeLastName = this.handleChangeLastName.bind(this);
-    this.handleChangeEmail = this.handleChangeEmail.bind(this);
-  }
+  // constructor(){
+  //   super()
+  //   this.state = {
+  //     //first view
+  //     loanType: 'Home Purchase',
+  //     propertyType: 'Single Family Home',
+  //     //second view
+  //     city: '',
+  //     //third view
+  //     propToBeUsedOn: '',
+  //     //fourth view
+  //     found: "false",
+  //     //fifth view
+  //     realEstateAgent: "false",
+  //     //sixth view
+  //     downPayment: 0,
+  //     cost: 0,
+  //     //seventh view
+  //     credit: '',
+  //     //eigth view
+  //     history: '',
+  //     //ninth view
+  //     addressOne: '',
+  //     addressTwo: '',
+  //     addressThree: '',
+  //     //tenth view
+  //     firstName: '',
+  //     lastName: '',
+  //     email: ''
+  //   }
+  //   //first view 
+  //   this.handleChangeLoanType = this.handleChangeLoanType.bind(this);
+  //   this.handleChangePropertyType = this.handleChangePropertyType.bind(this);
+  //   //second view
+  //   this.handleChangeCity = this.handleChangeCity.bind(this);
+  //   //third view
+  //   this.handleChangePropertyToBeUsedOn = this.handleChangePropertyToBeUsedOn.bind(this);
+  //   //fourth view
+  //   this.handleChangeFoundFalse = this.handleChangeFoundFalse.bind(this);
+  //   this.handleChangeFoundTrue = this.handleChangeFoundTrue.bind(this);
+  //   //fifth view
+  //   this.handleChangeRealEstateAgentTrue = this.handleChangeRealEstateAgentTrue.bind(this);
+  //   this.handleChangeRealEstateAgentFalse = this.handleChangeRealEstateAgentFalse.bind(this);
+  //   //sixth view
+  //   this.handleChangeUpdateDownPayment = this.handleChangeUpdateDownPayment.bind(this);
+  //   this.handleChangeUpdateCost = this.handleChangeUpdateCost.bind(this);
+  //   //seventh view
+  //   this.handleChangeCreditE = this.handleChangeCreditE.bind(this);
+  //   this.handleChangeCreditG = this.handleChangeCreditG.bind(this);
+  //   this.handleChangeCreditF = this.handleChangeCreditF.bind(this);
+  //   this.handleChangeCreditP = this.handleChangeCreditP.bind(this);
+  //   //eigth view
+  //   this.handleChangeUpdateHistory = this.handleChangeUpdateHistory.bind(this);
+  //   //ninth view
+  //   this.handleChangeAddressOne = this.handleChangeAddressOne.bind(this);
+  //   this.handleChangeAddressTwo = this.handleChangeAddressTwo.bind(this);
+  //   this.handleChangeAddressThree = this.handleChangeAddressThree.bind(this);
+  //   //tenth view
+  //   this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+  //   this.handleChangeLastName = this.handleChangeLastName.bind(this);
+  //   this.handleChangeEmail = this.handleChangeEmail.bind(this);
+  // }
     //first view
     handleChangeLoanType(event) {
         this.setState({loanType : event.target.value});
@@ -144,13 +145,18 @@ class App extends Component {
     return (
       <div>
     
-        { 
-          router( this.state.loanType, this.state.propertyType, this.handleChangeLoanType, this.handleChangePropertyType, this.handleChangePropertyToBeUsedOn, this.state.propToBeUsedOn, this.state.city, this.handleChangeCity, this.handleChangeFoundFalse, this.handleChangeFoundTrue, this.state.found, this.handleChangeRealEstateAgentTrue, this.handleChangeRealEstateAgentFalse, this.state.realEstateAgent, this.handleChangeUpdateDownPayment, this.state.downPayment, this.handleChangeUpdateCost, this.state.cost, this.state.credit, this.handleChangeCreditE, this.handleChangeCreditG,this.handleChangeCreditF, this.handleChangeCreditP, this.state.history, this.handleChangeUpdateHistory, this.state.addressOne, this.state.addressTwo, this.state.addressThree, this.handleChangeAddressOne, this.handleChangeAddressTwo, this.handleChangeAddressThree, this.handleChangeFirstName, this.handleChangeLastName, this.handleChangeEmail, this.state.firstName, this.state.lastName, this.state.email ) 
-        }
+        { router }
 
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps( state ) {
+  return {
+    state
+  };
+}
+
+
+export default connect( mapStateToProps )( App );
